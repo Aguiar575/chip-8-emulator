99fab737e41174f59ff802c19fc9bcefff32b702
Aguiar575 <aguiarus99arthurus@gmail.com>
2023-10-05 08:15:05 -0300

    updating build scritp to find the right sdl2 lib path

--- src/native_frotend/build.rs
+++ src/native_frotend/build.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("cargo:rustc-link-search=framework=/Users/arthuraguiar/Library/Frameworks")
+    println!("cargo:rustc-link-search=framework=/Library/Frameworks/");
+
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,/Library/Frameworks/");
+
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 }
